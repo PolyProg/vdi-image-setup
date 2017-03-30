b458292ef73a7cd80a11467bb772d59c2d06408b
Solal Pirelli <solal.pirelli@gmail.com>
2017-03-30 20:26:58 +0200

    HC2 firewall: Add Ubuntu repos

--- scripts/set-firewall-hc2.sh
+++ scripts/set-firewall-hc2.sh
@@ -7,9 +7,11 @@ if [ ! -f "set-firewall.sh" ]; then
 fi
 
 # In order:
+# - apt repositories
 # - VDI connection brokers
 # - VDI web access
 # - HC2 documentation, contest, and replica
-./set-firewall.sh itvdiconnect01.epfl.ch itvdiconnect02.epfl.ch itvdiconnect03.epfl.ch itvdiconnect04.epfl.ch \
+./set-firewall.sh ch.archive.ubuntu.com security.ubuntu.com repo.saltstack.com \
+                  itvdiconnect01.epfl.ch itvdiconnect02.epfl.ch itvdiconnect03.epfl.ch itvdiconnect04.epfl.ch \
                   itvdiweb01.epfl.ch itvdiweb02.epfl.ch vdi.epfl.ch \
                   doc.hc2.ch contest.hc2.ch repl.hc2.ch
