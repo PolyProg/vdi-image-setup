53c2b71a350981a397687c659eb2bea8780a0aeb
Solal Pirelli <solal.pirelli@gmail.com>
2017-08-15 23:02:35 -0700

    Update Eclipse script

--- scripts/add/software/eclipse.sh
+++ scripts/add/software/eclipse.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
-# Installs Eclipse Neon
-# TODO update (or better, find a way to always have the latest one)
+# Installs Eclipse 4.8 "Photon"
+# TODO can we autodetect the latest release?
 
-# Install Java first
+### Install Java first
 apt install -y openjdk-8-jdk
 
-# Download and extract it
-wget -O eclipse.tar.gz 'http://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/neon/2/eclipse-java-neon-2-linux-gtk-x86_64.tar.gz&r=1'
+
+### Install the Eclipse Platform Runtime, i.e. the shell without any plugins whatsoever
+
+# Download and extract platform
+wget -O eclipse.tar.gz 'http://www.eclipse.org/downloads/download.php?file=/eclipse/downloads/drops4/S-4.8M1-201708022000/eclipse-platform-4.8M1-linux-gtk-x86_64.tar.gz&r=1'
 tar -zxvf eclipse.tar.gz -C /opt
 rm eclipse.tar.gz
 
-# Install the program
+# TODO see if we need to patch eclipse.ini for memory or other
+
+# Install it
 ln -s /opt/eclipse/eclipse /usr/local/bin
 
-# Install the desktop shortcut
+# Create a desktop shortcut
 cat > /usr/share/applications/eclipse.desktop << EOF
 [Desktop Entry]
 Name=Eclipse
@@ -27,4 +32,26 @@ Categories=Development;IDE;
 Name[en]=Eclipse
 EOF
 
+# Install the shortcut
 desktop-file-install /usr/share/applications/eclipse.desktop
+
+
+### Add tools
+install() {
+  eclipse -nosplash -application org.eclipse.equinox.p2.director \
+          -repository $1 \
+          -installIU $2
+}
+
+cd /opt/eclipse
+
+# Java development tools
+install http://download.eclipse.org/releases/photon org.eclipse.jdt.feature.group
+
+# C/C++ development tools
+install http://download.eclipse.org/releases/photon org.eclipse.cdt.feature.group
+
+# PyDev, a.k.a. Python for Eclipse
+install http://www.pydev.org/updates org.python.pydev.feature.feature.group
+
+# TODO scala maybe?
