f306f3ceb6975739ee2ea65ad8dc556d1f9e4b4e
Solal Pirelli <solal.pirelli@gmail.com>
2017-10-04 22:04:02 -0700

    Install xterm for Code::Blocks

--- scripts/add/software/codeblocks.sh
+++ scripts/add/software/codeblocks.sh
@@ -3,4 +3,6 @@
 
 add-apt-repository -y ppa:damien-moore/codeblocks-stable
 apt update
-apt install -y codeblocks codeblocks-contrib
+# Code::Blocks uses xterm to run console programs by default
+# TODO figure out how to make it use the xfce terminal instead
+apt install -y codeblocks codeblocks-contrib xterm
