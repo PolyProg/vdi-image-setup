c281ad9abb33f713e3c39bb35967f28a86f65e81
Solal Pirelli <solal.pirelli@gmail.com>
2017-11-16 15:08:20 +0100

    Allow Javadoc in Eclipse.

--- install-hc2.sh
+++ install-hc2.sh
@@ -47,10 +47,12 @@ fi
 
 # Configure firewall, in order:
 # - apt repositories
+# - Oracle javadocs (required for Eclipse to use it)
 # - VDI connection brokers
 # - VDI web access
 # - HC2 saltmaster, documentation, contest, and replica
 ./configure/firewall.sh ch.archive.ubuntu.com security.ubuntu.com repo.saltstack.com \
+                        docs.oracle.com \
                         itvdiconnect01.epfl.ch itvdiconnect02.epfl.ch itvdiconnect03.epfl.ch itvdiconnect04.epfl.ch \
                         itvdiweb01.epfl.ch itvdiweb02.epfl.ch vdi.epfl.ch \
                         master.hc2.ch doc.hc2.ch contest.hc2.ch repl.hc2.ch
