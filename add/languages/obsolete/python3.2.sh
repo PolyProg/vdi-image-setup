841b3aa32071aee0e59694323f08da7cee7d23eb
Solal Pirelli <solal.pirelli@gmail.com>
2017-11-17 18:28:27 +0100

    Require software-properties-common at build time for scripts that need it

--- add/languages/obsolete/python3.2.sh
+++ add/languages/obsolete/python3.2.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 # Python 3.2 language support, latest available on contest.yandex.com as of 2016-11-16
 
-apt-get update
-apt-get install -y software-properties-common # for add-apt-repository
+NeedSpc=0
+if [ ! -x "$(command -v add-apt-repository)" ]; then
+  apt-get install -y software-properties-common
+  NeedSpc=1
+fi
+
+
 add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update
 apt-get install -y python3.2
+
+
+if [ $NeedSpc -eq 1 ]; then
+  apt-get purge --autoremove -y software-properties-common
+fi
--- add/software/codeblocks.sh
+++ add/software/codeblocks.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 # Installs Code::Blocks
 
+NeedSpc=0
+if [ ! -x "$(command -v add-apt-repository)" ]; then
+  apt-get install -y software-properties-common
+  NeedSpc=1
+fi
+
+
 add-apt-repository -y ppa:damien-moore/codeblocks-stable
 apt-get update
 # Code::Blocks uses xterm to run console programs by default
 # TODO figure out how to make it use the xfce terminal instead
 apt-get install -y codeblocks codeblocks-contrib xterm
+
+
+if [ $NeedSpc -eq 1 ]; then
+  apt-get purge --autoremove -y software-properties-common
+fi
