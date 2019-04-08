d6c7671848234f083a517a45641680719baa131b
Solal Pirelli <solal.pirelli@gmail.com>
2019-04-08 16:07:59 +0200

    Add gnupg for Java install

--- add/languages/java.sh
+++ add/languages/java.sh
@@ -4,6 +4,10 @@
 # See https://bugs.launchpad.net/ubuntu/+source/saaj/+bug/1814133
 # So we instead get a PPA from the Ubuntu OpenJDK team
 # Also we don't use add-apt-repository to install the PPA cause that conflicts with Python 3.7
+# TODO: This should just be apt-get install -y openjdk-11-jdk when they fix that...
+
+# Needed to install a key
+apt-get install -y gnupg
 
 echo 'deb http://ppa.launchpad.net/openjdk-r/ppa/ubuntu bionic main' >> '/etc/apt/sources.list'
 echo 'deb-src http://ppa.launchpad.net/openjdk-r/ppa/ubuntu bionic main' >> '/etc/apt/sources.list'
