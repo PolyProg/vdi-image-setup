87a8debc72538cb5be1326986e363829d69e2f94
Solal Pirelli <solal.pirelli@gmail.com>
2019-04-08 17:01:00 +0200

    Minor fix to VSCode

--- add/software/vscode.sh
+++ add/software/vscode.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # Installs Visual Studio Code
 
+apt-get install -y gnupg
 wget -O - 'https://packages.microsoft.com/keys/microsoft.asc' | gpg --dearmor > 'microsoft.gpg'
 install -o root -g root -m 644 'microsoft.gpg' '/etc/apt/trusted.gpg.d/'
 rm 'microsoft.gpg'
 echo 'deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main' > '/etc/apt/sources.list.d/vscode.list'
 
-apt-get install -y apt-transport-https
+# Note that Bionic doesn't need apt-transport-https any more
 apt-get update
 # VS Code requires gvfs-bin to delete files, according to its 'Common questions' section
 apt-get install -y code gvfs-bin
