dff16b4b2df2a42a57dd22cc99fae630c60ee8df
Solal Pirelli <solal.pirelli@gmail.com>
2019-04-09 09:40:45 +0200

    Fix command-not-found issue with Python 3.7

--- add/languages/python3.sh
+++ add/languages/python3.sh
@@ -4,3 +4,6 @@
 
 apt-get install -y python3.7
 update-alternatives --install '/usr/bin/python3' 'python3' '/usr/bin/python3.7' 999
+
+# Having 3.7 as the default causes issues with command-not-found during 'apt update', let's just remove it, no need for it anyway
+apt purge -y --autoremove command-not-found command-not-found-data python3-commandnotfound
